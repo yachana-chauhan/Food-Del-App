3fc7c6adba5a2fe2b06b748a1d3bda47b9427e4e
yachana-chauhan <yachanac23@gmail.com>
2025-12-25 20:19:45 +0530

    Update server.js

--- backend-part/server.js
+++ backend-part/server.js
@@ -20,15 +20,9 @@ app.use(express.json());
 app.use(cors({
     origin: [
         "http://localhost:5173", // frontend
-<<<<<<< HEAD
         "http://localhost:5174", // admin panel
           "https://neon-sopapillas-945e3a.netlify.app", //frontend
           "https://relaxed-tanuki-7b7956.netlify.app"
-=======
-        "http://localhost:5174",// admin panel
-        "https://neon-sopapillas-945e3a.netlify.app",
-        "https://relaxed-tanuki-7b7956.netlify.app"
->>>>>>> 88f83728142eb517162c935fddb28c8ca9184fa4
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
